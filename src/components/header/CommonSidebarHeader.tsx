817cda4c9a9d0d01bfda2b1c9ba30ed5f961aab7
nurhossainfarid <faahsan1516@gmail.com>
2025-02-15 10:58:27 +0600

    update active path styel

--- src/components/header/CommonSidebarHeader.tsx
+++ src/components/header/CommonSidebarHeader.tsx
@@ -2,9 +2,11 @@ import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { ModeToggle } from "../ModeToggle";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const CommonSidebarHeader = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
   return (
     <div className="h-20 bg-gray-800 text-white">
       {/*Menu Button */}
@@ -23,25 +25,36 @@ const CommonSidebarHeader = () => {
 
       {/*Menu */}
       {isOpen && (
-        <div className="absolute left-0 w-full bg-black dark:bg-gray-900 shadow-md p-4 flex flex-col gap-4 z-50">
-          <Link href="/" className="block py-2 px-4 hover:bg-gray-700 rounded">
+        <div className="absolute left-0 top-20 w-full bg-black/50 dark:bg-gray-900 shadow-md p-4 flex flex-col gap-4 z-50">
+          <Link
+            href="/"
+            className={`block py-2 px-4 hover:bg-gray-700 rounded ${
+              pathname === "/" ? "bg-gray-700" : ""
+            }`}
+          >
             Home
           </Link>
           <Link
             href="/projects"
-            className="block py-2 px-4 hover:bg-gray-700 rounded"
+            className={`block py-2 px-4 hover:bg-gray-700 rounded ${
+              pathname === "/projects" ? "bg-gray-700" : ""
+            }`}
           >
             Projects
           </Link>
           <Link
             href="/blogs"
-            className="block py-2 px-4 hover:bg-gray-700 rounded"
+            className={`block py-2 px-4 hover:bg-gray-700 rounded ${
+              pathname === "/blogs" ? "bg-gray-700" : ""
+            }`}
           >
             Blog
           </Link>
           <Link
             href="/contact"
-            className="block py-2 px-4 hover:bg-gray-700 rounded"
+            className={`block py-2 px-4 hover:bg-gray-700 rounded ${
+              pathname === "/contact" ? "bg-gray-700" : ""
+            }`}
           >
             Contact
           </Link>
